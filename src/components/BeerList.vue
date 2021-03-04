a744806abea87ebb19baebfda4b0ef8a34612507
Matthew <m.mccoach88@gmail.com>
2021-03-04 14:30:50 +0000

    Created skeletoned and registered the BeerListItem component

--- src/components/BeerList.vue
+++ src/components/BeerList.vue
@@ -1,9 +1,18 @@
 <template>
-  <h1>BeerList here</h1>
+  <div class="Beer List">
+    <h1>BeerList here</h1>
+    <beer-list-item/>
+  </div>
 </template>
 
 <script>
+import BeerListItem from './BeerListItem.vue'
+
+
 export default {
+  components: { 
+      'beer-list-item' : BeerListItem 
+  },
 
 }
 </script>
---
+++ src/components/BeerListItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>I'm a Beer List Item</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
