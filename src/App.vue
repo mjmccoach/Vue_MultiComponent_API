13d8b11b5cb2eb90c4201e12e8700cd2237273db
Matthew <m.mccoach88@gmail.com>
2021-03-04 14:38:21 +0000

    Created skeletoned and registed the FavouriteBeers component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <beer-list/>
     <beer-detail/>
+    <favourite-beers/>
   </div>
 </template>
 
 <script>
 import BeerList from './components/BeerList.vue';
 import BeerDetail from './components/BeerDetail.vue';
+import FavouriteBeers from './components/FavouriteBeers.vue';
 
 export default {
   name: 'App',
   components: {
     'beer-list' : BeerList,
-    'beer-detail' : BeerDetail
+    'beer-detail' : BeerDetail,
+    'favourite-beers' : FavouriteBeers
     
   }
 }
