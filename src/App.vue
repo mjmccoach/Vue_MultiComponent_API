2fbbe4c8f355ed0ff24338f287fdc7db80900c81
Matthew <m.mccoach88@gmail.com>
2021-03-04 14:34:02 +0000

    Created skeletoned and registered the BeerDetail component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <beer-list/>
+    <beer-detail/>
   </div>
 </template>
 
 <script>
 import BeerList from './components/BeerList.vue';
+import BeerDetail from './components/BeerDetail.vue';
 
 export default {
   name: 'App',
   components: {
-    'beer-list' : BeerList
+    'beer-list' : BeerList,
+    'beer-detail' : BeerDetail
     
   }
 }
